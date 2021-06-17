0705390444f4eb81a508c64304a52b8c7f505aa1
Shubham <chauhanshubham592@gmail.com>
2021-06-17 22:08:49 +0530

    Uploaded React Project

--- React Projects/Authentication Project/src/reducer/reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import authenticationReducer from './authenticationReducer'
-import userManagementReducer from './userManagementReducer'
-
-export default combineReducers({
-    authDetails: authenticationReducer,
-    userInfo: userManagementReducer
-})
